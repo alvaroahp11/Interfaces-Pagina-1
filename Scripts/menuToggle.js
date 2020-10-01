21ba52b5fa0326c361892abfcf2fc8eb0fcd0c03
Alvaro Henriquez <alvaroahp11@iMac.local>
2020-10-01 20:00:08 +0200

    casiListo

---
+++ Scripts/menuToggle.js
@@ -0,0 +1,12 @@
+jQuery('document').ready(function($){
+    var menuBtn = $('.menu-icon');
+    var menu = $('#menu');
+
+    menuBtn.click(function(){
+        if (menu.hasClass('show')){
+            menu.removeClass('show');
+        }else{
+            menu.addClass('show'); 
+        } 
+    });
+});
