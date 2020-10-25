57ee241bf736db396ad99d06db9d6987c613c37d
Gerardohb99 <gerardohb99@gmail.com>
2020-10-25 23:37:19 +0100

    Log in estructurado

--- Scripts/menuToggle.js
+++ Scripts/menuToggle.js
@@ -1,10 +1,40 @@
+footer = function(){ 
+
+    alto_navegador= $(window).height();
+    alto_documento= $(document).height(); 
+   
+    if (alto_documento>alto_navegador){
+        $('.footer').css({'position':"relative"});
+          
+    }
+    else{
+        $('.footer').css({'position':"fixed"});
+        
+    } 
+}
+
+togglePosition = function() {
+    var x = window.matchMedia("(max-width: 600px)");
+    if (x.matches && $('#iniciarSesion').css('display') != 'none') { // If media query matches 
+        if ($('.footer').css('position') == 'relative'){
+            $('.footer').css({'position':"fixed"});
+            
+        }else{
+            $('.footer').css({'position':"relative"});
+        }
+    }
+    x.addListener(myFunction)
+    
+}
 
 jQuery('document').ready(function($){
+    footer();
     var menuBtn = $('#menu-icon');
     var menu = $('#menu');
 
     menuBtn.click(function(){
         menu.toggle(500);
+        togglePosition();
     });
 
     var notiEventBtn = $('.tablet-news');
