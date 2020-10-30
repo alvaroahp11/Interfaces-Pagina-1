6c4bd3bc16238f71c75ea9731eebabda2c8ec1b4
Gerardohb99 <gerardohb99@gmail.com>
2020-10-30 19:51:50 +0100

    after dead
    Co-authored-by: Alvaro Henriquez <100395805@alumnos.uc3m.es>

--- Scripts/menuToggle.js
+++ Scripts/menuToggle.js
@@ -1,8 +1,110 @@
+//fixed footer at the bottom
 footer = function(){ 
     alto_main= $(window).height() - $('.footer').height() - $('.header').height();
     $('#main').css('min-height',alto_main + 'px') ; 
 }
 
+    //create cookie
+    function setCookie(cname, cvalue, exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        var expires = "expires="+d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + "; SameSite=Lax; secure";       
+    }
+
+    //get cookie
+    function getCookie(cname) {
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for(var i = 0; i < ca.length; i++) {
+          var c = ca[i];
+          while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+          }
+          if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+          }
+        }
+        return "";
+    }
+    
+    //check
+    function checkCookie(cname) {
+        var user = getCookie(cname);
+        if (user != "") {
+          alert("Welcome again " + user);
+        } else {
+          user = prompt("Please enter your name:", "");
+          if (user != "" && user != null) {
+            setCookie("username", user, 365);
+          }
+        }
+    }
+
+    function getUserFromSerializedArray(alldata){
+        
+        const user1 = new user(alldata[0].value, alldata[1].value, alldata[2].value, 
+                                alldata[3].value, alldata[4].value, alldata[5].value, 
+                                alldata[6].value, alldata[7].value, alldata[8].value, 
+                                alldata[9].value, alldata[10].value, alldata[11].value
+        );
+        return user1;
+    }
+
+    class user{
+
+        constructor(usuario, nia, pass, name, lastname, email, fnacimiento, id, rol, carrera, universidad, idioma){
+            this.usuarios=usuario;
+            this.nia = nia;
+            this.pass = pass;
+            this.name= name;
+            this.lastname = lastname;
+            this.email= email;
+            this.fnacimiento = fnacimiento;
+            this.id = id;
+            this.rol = rol;
+            this.carrera = carrera;
+            this.idioma = idioma;
+            this.universidad = universidad;
+        }
+
+        get usuario (){
+            return this.usuario;
+        }
+        get nia (){
+            return this.nia;
+        }
+        get pass(){
+            return this.pass
+        }
+        get name (){
+            return this.name
+        }
+        get lastname (){
+            return this.lastname
+        }
+        get email(){
+            return this.email;
+        }
+        get fnacimiento(){
+            return this.fnacimiento
+        }
+        get id(){
+            return this.id
+        }
+        get rol(){
+            return this.rol
+        }
+        get carrera(){
+            return this.carrera
+        }
+        get idioma(){
+            return this.idioma
+        }
+        get universidad(){
+            return this.universidad 
+        }
+    }
 
 
 jQuery('document').ready(function($){
@@ -51,19 +153,15 @@ jQuery('document').ready(function($){
     });
 
 
-    var regSumitBtn = $('#sumitBtn');
+    var registrarseForm = $('#registrarseForm');
 
-    regSumitBtn.submit(function(){
-        allMid.hide();
-        iniciarSesionPrincipal.show()
-        // var d = new Date();
-        // var expires = d.getTime() + (3*24*60*60*1000); 
-
-        // document.cookie = "pepe=pepe;" + expires + ";path=/";
-        var regInf = $('#registrarseForm').serialize();
-        console.log(regInf);
+    registrarseForm.submit(function (e) { 
+        e.preventDefault();
+        var x = registrarseForm.serializeArray();
+        var user1 = getUserFromSerializedArray(x);
+        console.log(user1.toString());
+      
     });
-
 
 
 
